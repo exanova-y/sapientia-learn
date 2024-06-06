b3a45faa972c65129d91419a0babd6b2c180c3af
Yoyo <52893812+exanova-y@users.noreply.github.com>
2024-06-06 14:13:59 -0400

    searches arxiv

---
+++ quick_test.py
@@ -0,0 +1,4 @@
+from datetime import date, time, datetime
+
+start = datetime(2024, 1, 1)
+print(start)
--- review.py
+++ review.py
@@ -1,20 +1,44 @@
 import requests
 from newsapi import NewsApiClient
+from arxiv import Search, arxiv
+import unidecode
 
-newsapi_instance = NewsApiClient(api_key="") # make sure to replace
 
+# users can modify these parameters
 neurotech_wordlist = ["Brain-Computer Interfaces", "Neurofeedback", "Neuroprosthetics"]
-
-# users can modify these search queries
+search_news = True
+search_discord = False
+search_journals = True
 query_keywords = neurotech_wordlist[0]
 start_date_str = "2024-05-10" # format: YYYY-MM-DD
 end_date_str = "2024-05-15" # with the free plan you can only access news articles within a month
 
-news_results = newsapi_instance.get_everything(q=query_keywords, from_param=start_date_str, to=end_date_str, sort_by="relevancy", language="en") 
-print(f"number of articles found: {len(news_results['articles'])}")
+# initialize clients only once
+arxiv_instance = arxiv.Client() 
+newsapi_instance = NewsApiClient(api_key="") # make sure to replace
 
-for article in news_results['articles']:
-    print(f"Title:{article['title']}")
-    print(f"Description: {article['description']}")
-    print(f"Published At: {article['publishedAt']}")
-    print("=============")
+# newsapi uses an AND operation regarding keywords 
+if search_news:
+    # newsapi searches for the exact keywords
+    news_results = newsapi_instance.get_everything(q=query_keywords, from_param=start_date_str, to=end_date_str, sort_by="relevancy", language="en") 
+    articles = news_results['articles']
+    headlines = [article['title'] for article in articles]
+    published_dates = [article['publishedAt'] for article in articles]
+    
+    print(f"number of articles found: {len(news_results['articles'])}")
+
+    for i in range(len(headlines)):
+        print("title: {headlines[i]}")
+        print("description: {descriptions[i]}")
+        print("published at: {published_dates[i]}")
+
+if search_journals:
+    arxiv_search_query = f"all:+{query_keywords} AND submittedDate:[{start_date_str}, {end_date_str}]"
+    search = Search(query=arxiv_search_query, sort_by = arxiv.SortCriterion.SubmittedDate) # search object
+    results = arxiv_instance.results(search)
+
+    paper_results = list(results)
+    for paper in paper_results:
+        print(f"title: {paper.title}")
+        print(f"summary: {paper.summary}"s) # abstract
+        print(paper.published_date.strftime('%Y-%m-%d'))
